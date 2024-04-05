4f8cabc307e03921c05bd1192e3516e40b35de0c
jeisonj2019 <jeisonj2019@gmail.com>
2024-04-05 06:31:56 +0500

    Увеличена задержка

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,12 +12,13 @@ export class HeaderComponent implements AfterViewInit {
   @Output() headerHeightCalculated: EventEmitter<string> = new EventEmitter<string>();
   @ViewChild('headerElem') headerElem: ElementRef<HTMLDivElement>;
   ngAfterViewInit(): void {
+    // действительная высота элемента известна только после применения шрифтов
     setTimeout(() => {
       var header = this.headerElem.nativeElement;
       var headerHeight = header.offsetHeight+'px';
       document.documentElement.style.setProperty('--header-height', headerHeight);
       
-    }, 50);
+    }, 100);
   }
 
 }
